073d1471628fed03e963ee9e3d135d47e68dadbc
Jouissance Seeker <051.sys113@gmail.com>
2025-03-30 13:59:39 +0330

    chore: before add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./globals.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './globals.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <p className="font-bold text-red-200">hello world</p>
   </StrictMode>,
 );
