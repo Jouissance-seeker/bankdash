f7248926a243ecd68b5fa3857bf0132b4330b20a
Jouissance Seeker <051.sys113@gmail.com>
2025-03-30 14:03:20 +0330

    feat: add eslint config

---
+++ eslint.config.cjs
@@ -0,0 +1,14 @@
+import { defineConfig } from '@fullstacksjs/eslint-config';
+
+export default defineConfig(
+  {
+    typescript: true,
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {},
+  },
+  {
+    rules: {},
+  },
+);
